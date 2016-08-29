e3c8bea87e46f781c71b44fed023e7cdb84a5abd
Vilan Le <vilan.nguyen.le@gmail.com>
2016-08-29 12:02:35 -0700

    creating event js

--- js/create_event.js
+++ js/create_event.js
@@ -1,19 +1,38 @@
  function store(){
-     var address1 = document.getElementById("first_a");
-     var address2 = document.getElementById("second_a");
-     var event_title = document.getElementById("event_title");
-     var event_date = document.getElementById("event_date");
-     var breed = document.getElementById("breed");
-     var dog_size = document.getElementById("dog_size");
-     var price = document.getElementById("price");
-     var event_description = document.getElementById("event_description");
-     localStorage.setItem("first_a", address1.value);
-     localStorage.setItem("second_a", address2.value);
-     localStorage.setItem("event_title", event_title.value);
-     localStorage.setItem("event_date", event_date.value);
-     localStorage.setItem("breed", breed.value);
-     localStorage.setItem("dog_size", dog_size.value);
-     localStorage.setItem("price", price.value);
-     localStorage.setItem("event_description", event_description.value);
-    }
+   var address1 = $("#first_a").val();
+   var address2 = $("#second_a").val();
+   var event_title = $("#event_title").val();;
+   var breed = $("#breed").val();
+   var dog_size = $("#dog_size").val();
+   var price = $("#price").val();
+   var event_description = $("#event_description").val();
+   let event_date = (parseDate($("#event_date").val()));
 
+   let state = JSON.parse(localStorage.State);
+   let accounts = JSON.parse(localStorage.Accounts);
+
+   let newEvent = {
+     title: event_title,
+     date: event_date.toLocaleString('en-US'),
+     dateString: event_date.valueOf(),
+     time: '',
+     breed: '',
+     dogSize: '',
+     location: 'Pattys Pub, San Diego',
+     price: 'free',
+     description: 'Explore local bars with your pup.',
+     image: './img/pub.jpg',
+     going: [],
+     maybe: [],
+     hostedBy: 'Pattys Pub',
+     comments: []
+   };
+
+
+
+}
+
+function parseDate(s) {
+  var b = s.split(/\D/);
+  return new Date(b[0], --b[1], b[2]);
+}
